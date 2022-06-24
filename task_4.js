93178e338445bf09e31feb32597d7c063411048a
Swapnil Katiyar <swapnil240695@gmail.com>
2022-06-25 00:37:35 +0530

    First commit for task 4

---
+++ task_4.js
@@ -0,0 +1,10 @@
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'Hello 2';
+items[1].style.backgroundColor = 'yellow';
+for(var i=0; i<items.length; i++) {
+    items[i].style.backgroundColor = '#f4f4f4';
+    items[i].style.fontWeight = 'bold';
+}
+items[2].style.backgroundColor = 'green';
